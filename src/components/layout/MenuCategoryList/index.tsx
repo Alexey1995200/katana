7be98916562c167c024a751e824edc46815929d5
Alexey1995200 <alexeyl1995200@gmail.com>
2025-03-01 14:59:55 +0200

    new Fonts

--- src/components/layout/MenuCategoryList/index.tsx
+++ src/components/layout/MenuCategoryList/index.tsx
@@ -1,23 +1,29 @@
 import {Link} from "react-router-dom";
 import './styles.scss'
-import {cart, childIco, dessertIco, hotIco, pizzaIco, rollsIco, saladIco, setIco, snackIco, soupIco, sushiIco, veganIco, wokIco} from "../../../assets";
+import {cart} from "../../../assets";
 import {categoryList} from "../../const";
 
 let money = 512
 
 const MenuCategoryList = () => {
   return (
-    <div className="menuCategoryList">
-      {categoryList.map((category) => {
-        return (
-          <button className="menuCategory" key={category.title}>
-            <Link key={category.title} to={`${category.link}`} className='category-link'>
-              <img src={category.ico} alt=""/>
-              {category.title}
-            </Link>
-          </button>)
-      })}
-      <button className="menuCategory-cart" key={'cart'}>
+    <div className={"menuCategoryList__wrapper"}>
+      <div className="menuCategoryList">
+        {categoryList.map((category) => {
+          return (
+            <button className="menuCategory" key={category.title}>
+              <Link key={category.title} to={`${category.link}`} className='category-link'>
+                <img src={category.ico} alt=""/>
+                {category.title}
+              </Link>
+            </button>)
+        })}
+      </div>
+      <button
+        className="menuCategory-cart"
+        key={'cart'}
+        style={{flexShrink: "0"}}
+      >
         <Link key={'cart'} to={`cart`} className='category-link-cart'>
           <img src={cart} alt=""/>
           <div className="cart-detais">
@@ -27,7 +33,8 @@ const MenuCategoryList = () => {
         </Link>
       </button>
     </div>
-  );
+  )
+    ;
 };
 
 
